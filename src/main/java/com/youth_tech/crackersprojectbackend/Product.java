4af9937f813d50ee920a02003d1b6590b9254aed
Thavasi Ganesh <thavasiganesh03@gmail.com>
2025-09-30 17:01:05 +0530

    Entity and Repository created.

---
+++ src/main/java/com/youth_tech/crackersprojectbackend/Product.java
@@ -0,0 +1,64 @@
+package com.youth_tech.crackersprojectbackend;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name="fireworks_products")
+public class Product {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Matches AUTO_INCREMENT
+    private Integer id;
+
+    @Column(name = "product_name", nullable = false, length = 255)
+    private String productName;
+
+    @Column(name = "product_category", nullable = false, length = 255)
+    private String productCategory;
+
+    @Column(name = "rate_per", nullable = false, length = 255)
+    private String ratePer;
+
+    @Column(name = "net_rate", nullable = false, precision = 10, scale = 2)
+    private BigDecimal netRate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductCategory() {
+		return productCategory;
+	}
+
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+
+	public String getRatePer() {
+		return ratePer;
+	}
+
+	public void setRatePer(String ratePer) {
+		this.ratePer = ratePer;
+	}
+
+	public BigDecimal getNetRate() {
+		return netRate;
+	}
+
+	public void setNetRate(BigDecimal netRate) {
+		this.netRate = netRate;
+	} 
+}
--- src/main/java/com/youth_tech/crackersprojectbackend/Products.java
+++
@@ -1,8 +0,0 @@
-package com.youth_tech.crackersprojectbackend;
-
-import jakarta.persistence.*;
-@Entity
-@Table(name="fireworks_products")
-public class Products {
-
-}
